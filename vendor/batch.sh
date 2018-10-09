578d75aaf5b3ed3d2fb7b0e049b49e311f539ec0
tukiyo3 <tukiyo3@gmail.com>
2018-10-10 08:48:38 +0900

    update

---
+++ vendor/batch.sh
@@ -0,0 +1,5 @@
+for i in  \
+  *.txt
+do
+  sh ../../convert.sh $i
+done
--- vendor/say.sh
+++ vendor/say.sh
@@ -3,17 +3,19 @@ set -eu
 # 改行で少し間を開ける
 sed "s/$/。/g" ${TXTFILE} > _tmp.txt
 
+date
 echo "[convert] txt -> audio"
-say -f "_tmp.txt" -o "_tmp.aiff"
+say --file-format=m4af -f "_tmp.txt" -o "_tmp.m4a"
 
+date
 echo "[convert] audio -> video"
 ffmpeg -loglevel quiet -loop 1 \
- -i _tmp.png -i _tmp.aiff \
+ -i _tmp.png -i _tmp.m4a \
  -af volume=15dB \
- -shortest "_tmp.mp4"
+ -shortest "_tmp.avi"
 
-mv "_tmp.mp4" "【${AUTHR}】 ${TITLE} （青空文庫 - 機械朗読）.mp4"
+mv "_tmp.avi" "【${AUTHR}】 ${TITLE} （青空文庫 - 機械朗読）.avi"
 
-rm _tmp.txt _tmp.png _tmp.aiff
+rm _tmp.txt _tmp.png _tmp.m4a
 
-ls *.mp4
+ls *.avi
